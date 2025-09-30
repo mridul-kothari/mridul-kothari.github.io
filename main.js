852e246f02a6851ea502f676abc159d245dfb2e7
mridulkothari <94312259+mridul-kothari@users.noreply.github.com>
2025-09-30 13:05:43 -0500

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-
-function toggleNav(){
-  const nav = document.getElementById('nav');
-  nav.style.display = (nav.style.display === 'block') ? 'none' : 'block';
-}
-document.getElementById('year').textContent = new Date().getFullYear();
